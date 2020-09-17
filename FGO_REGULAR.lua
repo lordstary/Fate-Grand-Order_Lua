21c197ac274d7605669190892ef54536848ed66e
lordstary <59037572+lordstary@users.noreply.github.com>
2020-09-16 21:12:38 -0400

    Update FGO_REGULAR.lua

--- FGO_REGULAR.lua
+++ FGO_REGULAR.lua
@@ -4,9 +4,9 @@ GameRegion = "EN"
 --Script Configuration, check instructions in the documentation: https://29988122.github.io/Fate-Grand-Order_Lua/#/features
 --***************************************************************************
 --AutoRefill Stamina
-Refill_Enabled = 0
+Refill_Enabled = 1
 Refill_Resource = "All Apples"
-Refill_Repetitions = 0
+Refill_Repetitions = 50
 
 --AutoSupportSelection Defaults
 Support_SelectionMode = "first"
